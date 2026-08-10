164919e17d3f583efa0438b711e1af0dfd24c3fd
Carlito Baessa <carlito.baessa@gmail.com>
2026-08-10 18:18:28 -0300

    v154: renomeia o app para 'Finanças & Patrimônio' (título, PWA, splash, sidebar, TOTP)

--- sw.js
+++ sw.js
@@ -12,7 +12,7 @@
 // Para forçar todo mundo a baixar uma atualização, basta mudar CACHE_VERSION
 // (feito a cada nova versão publicada do app).
 
-const CACHE_VERSION = 'financas-v153';
+const CACHE_VERSION = 'financas-v154';
 const APP_SHELL = [
   './',
   './index.html',
